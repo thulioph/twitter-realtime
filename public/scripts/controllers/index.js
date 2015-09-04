0391e995c012d2973c35e4c6375923b1145fbbd8
thulioph <thulioph@gmail.com>
2015-09-04 01:04:09 -0300

    Adiciona socket.io pro angular.

--- public/scripts/controllers/index.js
+++ public/scripts/controllers/index.js
@@ -8,6 +8,150 @@
  * Controller of the twRealtime
  */
 angular.module('twRealtime')
-  .controller('IndexCtrl', function ($scope) {
-    $scope.msg = 'Hello!';
-  });
+  .controller('IndexCtrl', ['$scope',  function ($scope) {
+
+    var socket, geocoder, 
+        myLatlng, mapOptions, infowindow, map, 
+        marcadorPersonalizado, styles, styledMap, notification, options;
+
+    $scope.geolocation = function() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition($scope.showMap, $scope.errorGeolocation);
+      } else {
+        console.log('not supported');
+      }
+    };
+
+    $scope.showMap = function(position) {
+      myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      
+      mapOptions = {
+        zoom: 11,
+        center: myLatlng,
+        panControl: false,
+        mapTypeControlOptions: {
+          mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+        }
+      }
+
+      map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
+
+      marcadorPersonalizado = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        icon: 'https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-32.png',
+        title: 'Você está aqui!'
+      });
+
+      infowindow = new google.maps.InfoWindow({
+        content: 'Você está aqui!',
+        maxWidth: 700
+      });
+
+      infowindow.open(map, marcadorPersonalizado);
+
+      // Estilizando o mapa;
+      styles = [
+        {
+          stylers: [
+            { hue: "#41a7d5" },
+            { saturation: 60 },
+            { lightness: -20 },
+            { gamma: 1.51 }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [
+            { lightness: 100 },
+            { visibility: "simplified" }
+          ]
+        },
+        {
+          featureType: "road",
+          elementType: "labels"
+        }
+      ];
+
+      styledMap = new google.maps.StyledMapType(styles, {
+        name: "Mapa Style"
+      });
+
+      // Aplicando as configurações do mapa
+      map.mapTypes.set('map_style', styledMap);
+      map.setMapTypeId('map_style');
+
+      geocoder = new google.maps.Geocoder();
+    };
+
+    $scope.errorGeolocation = function(msg) {
+      console.log('Error, geolocation: ', msg);
+    };
+
+    // pega a localização do usuário e mostra no mapa
+    $scope.geolocation();
+
+    $scope.showMapTweet = function(text, name, image, endereco) {
+
+      geocoder.geocode({'address': endereco}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+
+          console.log('latlng: ', results[0].geometry.location.G, results[0].geometry.location.K);
+
+            marcadorPersonalizado = new google.maps.Marker({
+              position: new google.maps.LatLng(results[0].geometry.location.G, results[0].geometry.location.K),
+              map: map,
+              icon: image
+            });
+
+            marcadorPersonalizado.setMap(map);
+
+            infowindow = new google.maps.InfoWindow({
+              content: text,
+              maxWidth: 700
+            });
+
+            infowindow.open(map, marcadorPersonalizado);
+
+        } else {
+          alert('Geocoder falhou por conta de: ' + status);
+        }
+      });
+
+      $scope.openNofify(name, text, image);
+    };
+
+    $scope.openNofify = function(title, body, icon) {
+      options = {
+        body: body,
+        icon: icon
+      };
+
+      if (!("Notification" in window)) {
+        console.log('Seu navegador não suporta notificações.');
+      } else if (Notification.permission === 'granted') {
+        notification = new Notification(title, options);
+      } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function(permission) {
+          if (permission === 'granted') {
+            notification = new Notification(title, options);
+          }
+        });
+      }
+    };
+
+    // socket.io();
+    socket = io.connect();
+
+    socket.on('stream', function(tweetJSON){
+      var data = tweetJSON,
+          text = data.text,
+          name = data.name, 
+          image = data.image,
+          endereco = data.endereco;
+
+        $scope.showMapTweet(text, name, image, endereco);
+    });
+
+  }]);
