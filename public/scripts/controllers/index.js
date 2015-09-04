81a435bdcf981e7f04c3a4b738244438d8efd244
thulioph <thulioph@gmail.com>
2015-09-04 00:42:08 -0300

    Adiciona nova estrutura para suporte do angular.

---
+++ public/scripts/controllers/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twRealtime.controller:IndexCtrl
+ * @description
+ * # IndexCtrl
+ * Controller of the twRealtime
+ */
+angular.module('twRealtime')
+  .controller('IndexCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
