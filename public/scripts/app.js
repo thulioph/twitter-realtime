847a786051cc648f703ee1e4739c3707e244e5a7
thulioph <thulioph@gmail.com>
2017-10-10 10:35:05 -0300

    Updates the app.js to use doublequotes.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,22 @@
-'use strict';
+(function() {
+  "use strict";
 
-/**
- * @ngdoc overview
- * @name twRealtime
- * @description
- * # twRealtime
- *
- * Main module of the application.
- */
-angular
-  .module('twRealtime', [
-    'ngAnimate',
-    'ngResource',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/index.html',
-        controller: 'IndexCtrl',
-        controllerAs: 'index'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  angular
+    .module("twRealtime", [
+      "ngAnimate",
+      "ngResource",
+      "ngRoute"
+    ])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when("/", {
+          templateUrl: "views/index.html",
+          controller: "IndexCtrl",
+          controllerAs: "index"
+        })
+        .otherwise({
+          redirectTo: "/"
+        });
+    });
+})();
+
